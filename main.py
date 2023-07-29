ce75757971bb450fea6f510284e92fd87cd6c812
Tim Renken <tim@HOME.LOCAL>
2023-07-29 06:46:00 -0600

    added site variable

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def password_generator(username, length, secret_key):
+def password_generator(username, site, length, secret_key):
+
+    if site == None or site == "":
+
     # Concatenate the username and the secret key
-    input_string = username + secret_key
+        input_string = username + secret_key
+    
+    else:
+        input_string = username + site + secret_key
 
     # Use SHA256 to generate a deterministic hash
     hash_object = hashlib.sha256(input_string.encode('utf-8'))
 
     # Create a character set for the password
     chars = string.ascii_letters + string.digits + "!@#$%^&*(),.<>/?"
-    print(chars)
     # Convert the hash to a password using the character set
     binary_hash = hash_object.digest()
     password = [chars[b % len(chars)] for b in binary_hash]
@@ -28,7 +33,8 @@ def password_generator(username, length, secret_key):
 
 
 username = input("Username: ")
+site = input("Site: ").lower()
 length = int(input("Length: "))
 secret_key = os.getenv('secret_key')
 
-print(f"Password: {password_generator(username, length, secret_key)}")
+print(f"Password: {password_generator(username, site, length, secret_key)}")
